761d300dce1b7cca699f9c8c1ea36cfccdf5e785
Santiago Galfré <55896936+CazaBobos@users.noreply.github.com>
2022-02-14 23:25:46 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,23 +5,15 @@ const path = require('path'); //for \ or / conversion
 const { mongoose } = require('./database');
 const app = express();
 
-//#region SETTINGS
+//SETTINGS
 app.set('port', process.env.PORT || 9999);
-//#endregion
 
-//#region MIDDLEWARES 
+//MIDDLEWARES 
 app.use(morgan('dev'));
 app.use(express.json());
-//#endregion
 
-//#region ROUTES
+//ROUTES
 app.use('/api/tasks', require('./routes/task.routes'));
-//#endregion
-
-//#region STATIC FILES
-const location = path.join(__dirname, 'public');
-app.use(express.static(location));
-//#endregion
 
 //SERVER START
 const PORT = app.get('port');
